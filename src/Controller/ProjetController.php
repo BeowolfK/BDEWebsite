cf5843c771c63ecc3758b348fc89ba10c087148f
Kénan Meylan <kenan.meylan@gmail.com>
2022-05-18 14:55:55 +0200

    projet

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Projet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProjetController extends AbstractController
-{
+{   
     /**
      * @Route("/projet", name="app_projet")
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Projet::class);
+        $projets = $repo->findAll();
+
         return $this->render('projet/index.html.twig', [
             'controller_name' => 'ProjetController',
+            'projets' => $projets
         ]);
     }
 }
--- var/cache/dev/twig/d5/d53c0daa8edd215ae37d047ae7430f22a72e3be0e6b14fd78a1ebf49146df87f.php
+++ var/cache/dev/twig/d5/d53c0daa8edd215ae37d047ae7430f22a72e3be0e6b14fd78a1ebf49146df87f.php
@@ -25,7 +25,6 @@ class __TwigTemplate_223b62585b41b72286e0790e84ff5497750139aba3de7dc0cbc46d107ff
         $this->source = $this->getSourceContext();
 
         $this->blocks = [
-            'title' => [$this, 'block_title'],
             'body' => [$this, 'block_body'],
         ];
     }
@@ -56,25 +55,6 @@ class __TwigTemplate_223b62585b41b72286e0790e84ff5497750139aba3de7dc0cbc46d107ff
     }
 
     // line 3
-    public function block_title($context, array $blocks = [])
-    {
-        $macros = $this->macros;
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
-
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
-
-        echo "Hello ProjetController!";
-        
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
-
-        
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
-
-    }
-
-    // line 5
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -84,30 +64,48 @@ class __TwigTemplate_223b62585b41b72286e0790e84ff5497750139aba3de7dc0cbc46d107ff
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 6
-        echo "<style>
-    .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
-    .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
-</style>
+        // line 4
+        echo "
+";
+        // line 5
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["projets"]) || array_key_exists("projets", $context) ? $context["projets"] : (function () { throw new RuntimeError('Variable "projets" does not exist.', 5, $this->source); })()));
+        foreach ($context['_seq'] as $context["_key"] => $context["projet"]) {
+            // line 6
+            echo "    <projet>
+        <h2>";
+            // line 7
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["projet"], "titre", [], "any", false, false, false, 7), "html", null, true);
+            echo "</h2>
+        <div> Ecrit le ";
+            // line 8
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["projet"], "date", [], "any", false, false, false, 8), "d/m/Y"), "html", null, true);
+            echo " à ";
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["projet"], "date", [], "any", false, false, false, 8), "H:i"), "html", null, true);
+            echo "</div>
+        <img src=\"";
+            // line 9
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["projet"], "photo", [], "any", false, false, false, 9), "html", null, true);
+            echo "\">
+        <p>";
+            // line 10
+            echo twig_get_attribute($this->env, $this->source, $context["projet"], "description", [], "any", false, false, false, 10);
+            echo "</p>
+        <a href=\"#\">";
+            // line 11
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["projet"], "id", [], "any", false, false, false, 11), "html", null, true);
+            echo "</a>
 
-<div class=\"example-wrapper\">
-    <h1>Hello ";
-        // line 12
-        echo twig_escape_filter($this->env, (isset($context["controller_name"]) || array_key_exists("controller_name", $context) ? $context["controller_name"] : (function () { throw new RuntimeError('Variable "controller_name" does not exist.', 12, $this->source); })()), "html", null, true);
-        echo "! ✅</h1>
+    </projet>
 
-    This friendly message is coming from:
-    <ul>
-        <li>Your controller at <code><a href=\"";
-        // line 16
-        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\CodeExtension']->getFileLink("/amuhome/m20014866/webdev/BDE/src/Controller/ProjetController.php", 0), "html", null, true);
-        echo "\">src/Controller/ProjetController.php</a></code></li>
-        <li>Your template at <code><a href=\"";
+
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['projet'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
         // line 17
-        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\CodeExtension']->getFileLink("/amuhome/m20014866/webdev/BDE/templates/projet/index.html.twig", 0), "html", null, true);
-        echo "\">templates/projet/index.html.twig</a></code></li>
-    </ul>
-</div>
+        echo "
 ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -129,30 +127,28 @@ class __TwigTemplate_223b62585b41b72286e0790e84ff5497750139aba3de7dc0cbc46d107ff
 
     public function getDebugInfo()
     {
-        return array (  107 => 17,  103 => 16,  96 => 12,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
+        return array (  108 => 17,  96 => 11,  92 => 10,  88 => 9,  82 => 8,  78 => 7,  75 => 6,  71 => 5,  68 => 4,  58 => 3,  35 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Source("{% extends 'base.html.twig' %}
 
-{% block title %}Hello ProjetController!{% endblock %}
-
 {% block body %}
-<style>
-    .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
-    .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
-</style>
 
-<div class=\"example-wrapper\">
-    <h1>Hello {{ controller_name }}! ✅</h1>
+{% for projet in projets %}
+    <projet>
+        <h2>{{ projet.titre }}</h2>
+        <div> Ecrit le {{ projet.date | date('d/m/Y') }} à {{ projet.date | date('H:i') }}</div>
+        <img src=\"{{ projet.photo }}\">
+        <p>{{ projet.description | raw}}</p>
+        <a href=\"#\">{{ projet.id }}</a>
 
-    This friendly message is coming from:
-    <ul>
-        <li>Your controller at <code><a href=\"{{ '/amuhome/m20014866/webdev/BDE/src/Controller/ProjetController.php'|file_link(0) }}\">src/Controller/ProjetController.php</a></code></li>
-        <li>Your template at <code><a href=\"{{ '/amuhome/m20014866/webdev/BDE/templates/projet/index.html.twig'|file_link(0) }}\">templates/projet/index.html.twig</a></code></li>
-    </ul>
-</div>
+    </projet>
+
+
+{% endfor %}
+
 {% endblock %}
 ", "projet/index.html.twig", "/amuhome/m20014866/webdev/BDE/templates/projet/index.html.twig");
     }
